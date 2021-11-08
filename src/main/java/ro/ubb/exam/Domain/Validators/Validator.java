8103082714ce045c5b9177b3b3934af44e601aa4
cristinaflonta <cris.flonta@gmail.com>
2021-11-08 20:29:21 +0200

    modificari

--- src/main/java/ro/ubb/exam/Domain/Validators/Validator.java
+++
@@ -1,9 +0,0 @@
-package ro.ubb.exam.Domain.Validators;
-
-import ro.ubb.exam.Domain.Entity;
-
-public abstract class Validator<T extends Entity>  {
-
-    public abstract void validate(T entity);
-
-}
